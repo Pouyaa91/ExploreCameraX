1b3badb627b412e85e736d85439669c58f8ecdf2
Pouya Mazloumpour <mazloumpour.pouya@gmail.com>
2023-10-24 11:22:38 -0700

    feat: show landmark analyzer view

--- app/src/main/java/com/example/explorecamerax/MainActivity.kt
+++ app/src/main/java/com/example/explorecamerax/MainActivity.kt
@@ -1,48 +1,49 @@
 package com.example.explorecamerax
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.example.explorecamerax.data.LandmarkDetector
+import com.example.explorecamerax.ui.LandmarkAnalyzerScreen
 import com.example.explorecamerax.ui.theme.ExploreCameraXTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var landmarkDetector: LandmarkDetector
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setKeepScreenOn()
+        requestCameraPermission()
         setContent {
             ExploreCameraXTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                LandmarkAnalyzerScreen(
+                    landmarkDetector = landmarkDetector
+                )
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun setKeepScreenOn() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExploreCameraXTheme {
-        Greeting("Android")
+    private fun requestCameraPermission() {
+        // TODO: Replace with the proper way for requesting permissions
+        if (hasCameraPermission()) return
+        requestPermissions(arrayOf(Manifest.permission.CAMERA), 0)
+    }
+
+
+    private fun hasCameraPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        return result == PackageManager.PERMISSION_GRANTED
     }
 }
